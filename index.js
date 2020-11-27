452eff0683c3ab9d27275ed3d6d7da9b03b4fdb3
sebastien <sebastien.laneres@gmail.com>
2020-11-27 23:28:50 +0100

    index.js

---
+++ index.js
@@ -0,0 +1,24 @@
+const config = require('config');
+const express = require("express");
+const app = express();
+
+// if (!config.get('jwtPrivateKey')) {
+//   console.error('FATAL ERROR: jwtPrivateKey is not defined');
+//   process.exit(1);
+// }
+
+require('./startup/logging');
+require('./startup/db')();
+require('./startup/routes')(app);
+
+console.log(`NODE_ENV: ${process.env.NODE_ENV}`);
+
+if(app.get('env') === "production"){
+  require('./startup/prod')(app);
+}
+
+const port = process.env.PORT || 8080;
+const server = app.listen(port, () => console.log(`Listening on port ${port}...`));
+
+module.exports = server;
+
